529f30fed4b2a43379dbb7c1392e4e3bf8ec2f21
koded <koded@ixit.com.ng>
2019-05-24 09:43:10 +0100

    completed objects task

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.call(obj)
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting
+      return constructor
     },
 
     iterate : function(obj) {
-
+      let m = []
+      for (let t in obj) {
+        if (obj.hasOwnProperty(t)) {
+          m.push(`${t}: ${obj[t]}`)
+        }
+      }
+      return m
     }
   };
 });
