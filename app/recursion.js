e06bbd4c40268c30550c012522cd044f12f7527c
koded <koded@ixit.com.ng>
2019-05-25 06:58:54 +0100

    - thnkg God ds wasnt hard

--- app/recursion.js
+++ app/recursion.js
@@ -31,8 +31,24 @@ define(function() {
         return filelist;
     },
 
-    permute: function(arr) {
+    permute: function(inputArr) {
+      let result = [];
 
+      const permute = (arr, m = []) => {
+        if (arr.length === 0) {
+          result.push(m)
+        } else {
+          for (let i = 0; i < arr.length; i++) {
+            let curr = arr.slice();
+            let next = curr.splice(i, 1);
+            permute(curr.slice(), m.concat(next))
+         }
+       }
+     }
+    
+     permute(inputArr)
+    
+     return result;
     }
   };
 });
